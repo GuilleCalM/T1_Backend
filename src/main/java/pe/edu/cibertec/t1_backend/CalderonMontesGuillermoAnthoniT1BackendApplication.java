3b2526b71647f475cfb4d52ea73a41852785a777
GuilleCalM <guilleacalderon@hotmail.com>
2024-09-22 18:30:30 -0500

    Commit inicial del proyecto T1_Backend

---
+++ src/main/java/pe/edu/cibertec/t1_backend/CalderonMontesGuillermoAnthoniT1BackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalderonMontesGuillermoAnthoniT1BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalderonMontesGuillermoAnthoniT1BackendApplication.class, args);
+    }
+
+}
